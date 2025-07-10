dda74a96b0f28a75a3f0cf9c2407592adb731218
sharmahusky <sharmahusky5@gmail.com>
2025-07-10 08:40:32 +0530

    moved logo in public folder

--- src/data/company.js
+++ src/data/company.js
@@ -12,7 +12,7 @@ export const companyInfo = {
     address: "Plot No. 4906/B, GIDC Estate, Ankleshwar, Gujarat, India"
   },
   
-  logo: "/src/assets/logo.svg",
+  logo: "/logo.svg",
   
   hero: {
     backgroundImage: "https://images.unsplash.com/photo-1567427018141-0584cfcbf1b8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
